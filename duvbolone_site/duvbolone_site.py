2ef1efb98cbeadc474204a1a1694d07bec047411
Matt3o0 <duvbolone@gmail.com>
2023-02-15 23:05:55 +0100

    Updates

--- duvbolone_site/duvbolone_site.py
+++ duvbolone_site/duvbolone_site.py
@@ -2,15 +2,8 @@ from pcconfig import config
 import pynecone as pc
 from duvbolone_site.pages import index, about
 
-def toggleColor():
-        pc.toggle_color_mode
-
 class State(pc.State):
-    theme = "light"
-
-    def toggleColor(self):
-        pc.toggle_color_mode
-        #self.theme = not self.theme
+    pass
 
 
 app = pc.App(
@@ -18,6 +11,6 @@ app = pc.App(
     )
 
 # Add state and page to the app.
-app.add_page(index.index, route="/", title="Matt3o0's Website", image="/M27.png", description="My website. Caution, it's very questionable 💀", on_load=toggleColor)
-app.add_page(about.about, route="/about", title="Matt3o0's Website", image="/M27.png", description="My website. Caution, it's very questionable 💀", on_load=toggleColor)
+app.add_page(index.index, route="/", title="Matt3o0's Website", image="/M27.png", description="My website. Caution, it's very questionable 💀")
+app.add_page(about.about, route="/about", title="Matt3o0's Website", image="/M27.png", description="My website. Caution, it's very questionable 💀")
 app.compile()
