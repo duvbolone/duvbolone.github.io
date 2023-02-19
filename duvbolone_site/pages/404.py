c97683890ae963ac571d9fcb78152e40b9ff293a
Matt3o0 <duvbolone@gmail.com>
2023-02-19 12:53:54 +0100

    Fix error 404 page

--- duvbolone_site/pages/404.py
+++ duvbolone_site/pages/404.py
@@ -1,23 +1,19 @@
 import pynecone as pc
 from duvbolone_site.comps.navbar import navBar
 from duvbolone_site.comps.footbar import footBar
-from duvbolone_site.comps.state import State
 
-route = "/[any]"
+route = "/404"
 title = "404 - Matt3o0's Website"
 image = "/M27.png"
-description = "This is a 404 page, nothing here 💀"
+description = "This is a 404 page, nothing interesting here 💀"
 
 
 def page():
-    url = State.get_url
     return pc.box(
         pc.vstack(
             pc.heading("Error 404", font_size="900%", padding="30px"),
             pc.hstack(
-                pc.heading("Page", color="rgb(220, 220, 220)"),
-                pc.heading(url, color="rgb(160, 160, 160)"),
-                pc.heading("not found", color="rgb(220, 220, 220)")
+                pc.heading("Page not found", color="rgb(220, 220, 220)"),
             ),
             pc.spacer(padding="50px"),
             pc.hstack(
